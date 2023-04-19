28cc843a8dc896fb1f7fa258557b906140ecdcbd
nurettinyavuz <Nurettinyavuz.js@gmail.com>
2023-04-19 18:40:45 +0300

    Public klasörü oluşturup statik dosyalarımızı içerisine yerleştirelim. İlgili middleware fonksiyonunu yazarak public klasörümüzü uygulamamıza kaydedelim. EJS modülünü indirelim. Uygulamamızda EJS modülünü kullanacağımızı belirtelim. Views klasörü oluşturalım ve tüm .html dosyalarımız views klasörü içerisinde .ejs dosyalarına çevirelim. Partials klasör yapısını oluşturalım. İlgili yönlendirmeleri ve _navigation.ejs klasöründeki link düzenlemelerini yapalım.

--- Clean Blog Project/app.js
+++ Clean Blog Project/app.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    const blog ={
-        id: 1, 
-        title: "Blog title", 
-        description: "Blog description"
-    }
-    res.send(blog);
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
 const port = 3000;
